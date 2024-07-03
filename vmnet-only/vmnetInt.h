2da81829a3c8ad56529bd69419768bdef9df54f4
Bytium <info@bytium.com>
2024-07-03 17:13:38 +0600

    VM Workstation 17.5.2

--- vmnet-only/vmnetInt.h
+++ vmnet-only/vmnetInt.h
@@ -41,9 +41,10 @@
     compat_skb_set_network_header(skb, sizeof (struct ethhdr)),  \
     dev_queue_xmit(skb)                                   \
   )
-#define dev_lock_list()    read_lock(&dev_base_lock)
-#define dev_unlock_list()  read_unlock(&dev_base_lock)
-
+//#define dev_lock_list()    read_lock(&dev_base_lock)
+//#define dev_unlock_list()  read_unlock(&dev_base_lock)
+#define dev_lock_list()    rcu_read_lock()
+#define dev_unlock_list()  rcu_read_unlock()
 
 extern struct proto vmnet_proto;
 #if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 2, 0) || defined(sk_net_refcnt)
